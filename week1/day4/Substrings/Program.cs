6e8fb22d6d4413b611d0ae5bf8b28fb2b2944a4a
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-02-27 16:17:27 -0600

    placed back week 1 exercises; should ignore bin and obj dlls

---
+++ week1/day4/Substrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Substrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetSubstring();
+        }
+
+        static void Substring(string str, int num)
+        {
+            // for loop
+            for (int i = 0; i < (str.Length - num + 1); i++)    // loop stops one too early without + 1 on num
+                                                                // (index starts at 0, not 1)
+            {
+                Console.WriteLine(str.Substring(i, num));
+
+            }
+        }
+
+        static void GetSubstring()
+        {
+            System.Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+            System.Console.WriteLine("Enter a whole number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Substring(str, num);
+        }
+
+
+    }
+}
