200748ebad9267e383b18d2bb096b0a2986509ce
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-02-27 16:05:27 -0600

    added back c# exercises now that .gigignore is active; dll files should no longer get uploaded to github

---
+++ week1/day3/StringStuff/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StringStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some text: ");
+            string input = Console.ReadLine();
+            string reversed = ReverseString(input);
+
+            Console.WriteLine("Reversed: " + reversed);
+            
+        }
+
+        static string ReverseString(string input)
+        {
+            string process = input;
+            string final = ""; //empty
+
+            foreach (char i in process)
+            {
+                string end = process.Substring( process.Length - 1); //get last character
+                //final= final + end;    // place in final at start
+                //Console.WriteLine("process start " + process);
+                //Console.WriteLine("end is " + end);
+                final += end;
+                process = process.Remove(process.Length - 1); // remove last char for loop
+                //Console.WriteLine("process end " + process);
+                //Console.WriteLine("final in progress: " + final);
+
+
+
+            }
+            return final;
+        }
+    }
+}
