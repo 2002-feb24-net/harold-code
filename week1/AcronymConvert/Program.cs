63364a6668e5b3c13bd1087613682c3301550c16
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-02-28 09:38:44 -0600

    New program: Enter a full word, returns an acronym to console

--- week1/AcronymConvert/Program.cs
+++ week1/AcronymConvert/Program.cs
@@ -6,7 +6,33 @@ namespace AcronymConvert
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input;
+            string output;
+            input = GetInput();
+            output = GenerateAcronym(input);
+            Console.WriteLine("Acronym: " + "\n" +
+            output);
+        }
+
+        static string GetInput()
+        {
+            System.Console.WriteLine("Enter a word to turn into an acronym:" + "\n" +
+            "ex: central processing unit");
+            return Console.ReadLine();
+        }
+
+        static string GenerateAcronym(string fullWord)
+        {
+            string [] splitWords = fullWord.Split(' ');
+            string finalAcronym = "";   //empty string for operation
+            for (int i = 0; i < splitWords.Length; i++)
+            {
+                string letter = splitWords[i].Substring(0, 1);  //get first char of each letter
+                letter = letter.ToUpper();  // capitalize each letter for acronym
+                finalAcronym += letter;
+
+            }
+            return finalAcronym;
         }
     }
 }
